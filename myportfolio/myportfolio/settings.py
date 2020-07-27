c6a7943759a3346d5f118361ab60949bf8fb25b1
nish8690 <nishant.shah0806@gmail.com>
2020-07-27 23:44:38 +0000

    update projecct

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7yg+)kvr!hb$vp&j0p-w5m5mcbjx!j3hcz-44(@st$q+adr_+5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
